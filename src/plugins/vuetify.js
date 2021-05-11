b9db96bdbc165c00b8a2f1a24d07d693c703d385
N3kox <1377648701@qq.com>
2021-05-11 10:55:30 +0800

    5.11 updates

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -16,19 +16,22 @@ const messages = {
       examplesButton: 'Enter',
       sidebar: {
         title1: {
-          value: 'SearchView',
+          value: '总览',
           children: {
             title1: {
-              value: 'MissionTicketOverview'
+              value: '工单总览'
             },
             title2: {
-              value: 'DeviceOverview'
+              value: '设备总览'
             },
             title3: {
-              value: 'SearchView'
+              value: '定向搜索'
             },
             title4: {
-              value: 'TermSearch'
+              value: '术语搜索'
+            },
+            title5: {
+              value: '数据上传x'
             }
           }
         },
@@ -47,129 +50,7 @@ const messages = {
     $vuetify: {
       examplesButton: '进入',
       sidebar: {
-        title1: {
-          value: '基础',
-          children: {
-            title1: {
-              value: '你好，世界'
-            },
-            title2: {
-              value: '更新、添加、退出'
-            },
-            title3: {
-              value: '生成、更新 I'
-            },
-            title4: {
-              value: '选择元素并绑定数据'
-            },
-            title5: {
-              value: '选择、添加、移除'
-            }
-          }
-        },
-        title2: {
-          value: '树',
-          children: {
-            title1: {
-              value: '树 I'
-            },
-            title2: {
-              value: '树 II'
-            },
-            title3: {
-              value: '树 III'
-            },
-            title4: {
-              value: '树 IV'
-            },
-            title5: {
-              value: '树 V'
-            },
-            title6: {
-              value: '树 VI'
-            },
-            title7: {
-              value: '树 VII'
-            },
-            title8: {
-              value: '树 VIII'
-            }
-          }
-        },
-        title3: {
-          value: '条形图',
-          children: {
-            title1: {
-              value: '条形图 I'
-            },
-            title2: {
-              value: '条形图 II'
-            },
-            title3: {
-              value: '坐标轴 条形图'
-            },
-            title4: {
-              value: '简单条形图'
-            },
-            title5: {
-              value: '比例尺'
-            }
-          }
-        },
-        title4: {
-          value: '缩放',
-          children: {
-            title1: {
-              value: '可缩放的力导向图'
-            },
-            title2: {
-              value: '缩放,显示节点文本'
-            }
-          }
-        },
-        title5: {
-          value: '力导向图',
-          children: {
-            title1: {
-              value: '力导向图 I'
-            },
-            title2: {
-              value: '力导向图 II'
-            },
-            title3: {
-              value: '力导向图 III'
-            },
-            title4: {
-              value: '力导向图 关系'
-            },
-            title5: {
-              value: '力导向图 布局 I'
-            },
-            title6: {
-              value: '力导向图 布局 II'
-            },
-            title7: {
-              value: '力导向图 布局 III'
-            },
-            tile8: {
-              value: 'D3Network'
-            }
-          }
-        },
-        title6: {
-          value: '直方图',
-          children: {
-            title1: {
-              value: '直方图 I'
-            },
-            title2: {
-              value: '直方图 II'
-            },
-            title3: {
-              value: '直方图 III'
-            }
-          }
-        }
+        
       }
     }
   }
